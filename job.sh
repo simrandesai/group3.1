aa7ed851e744d3a4e237b88c6078ed5442b75102
Aurora Toutant <atoutant@wisc.edu>
2024-04-06 15:36:54 -0500

    added job.sub, job.sh, and inputFileList for part 2

---
+++ job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "usage: $0 <file>"
+    exit 0
+fi
+
+file=$1
+
+output_file="${input_file}.processed"
+
+tr '[:upper:]' '[:lower:]' < $file | sed 's/[[:punct:]]//g' | tr -s '[:blank:]' '\n' |grep -vE '^\s*$' | sort > $output_file
+
+echo "Processed file: $output_file"
