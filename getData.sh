bb934aad86cff97138abc28a15e16345a132ba12
Aurora Toutant <atoutant@wisc.edu>
2024-04-07 12:50:18 -0500

    changed getData.sh

--- getData.sh
+++ getData.sh
@@ -3,7 +3,10 @@
 wget -O shakespeare.tar https://pages.stat.wisc.edu/~jgillett/DSCP/CHTC/wordCounting/shakespeare.tar
 tar -xf shakespeare.tar
 
-cat shakespeare/*.txt > shakespeare_all.txt
+find shakespeare/ -type f -not -name "README" -exec cat {} + > shakespeare_all.txt
+
+#this wouldn't work for me so I made a different line (the one above) keeping yours just incase
+#cat shakespeare/*.txt > shakespeare_all.txt
 
 split -n l/5 -a 2 -d shakespeare_all.txt file
 
