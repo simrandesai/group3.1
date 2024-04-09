af233872c2478194fae9d4a6789f71500d34b6c8
Lexi Siegel <avsiegel.wisc.edu>
2024-04-09 13:20:18 -0500

    revised post

--- post.sh
+++ post.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sort -m < "file00.processed" "file01.processed" "file02.processed" "file03.processed" "file04.processed" | uniq -c > "countOfWords"
+#sort -m < "file00.processed" "file01.processed" "file02.processed" "file03.processed" "file04.processed" | uniq -c > "countOfWords"
+
+sort -m "file00.processed" "file01.processed" "file02.processed" "file03.processed" "file04.processed" > merged_sorted_file
+
+uniq -c merged_sorted_file | sort -nr > countsOfWords
+
+
